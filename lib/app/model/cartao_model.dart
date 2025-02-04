e16d154a78f69251eee0cfb330556c1d543d849a
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-02-04 15:53:53 -0300

    feat: alteração dos atributos de CartaoModel

--- lib/app/model/cartao_model.dart
+++ lib/app/model/cartao_model.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/material.dart';
+
 class CartaoModel {
+  final int id;
   final String number;
   final String owner;
   final double limit;
   final int bestDay;
+  final List<Color> cardColor;
 
   CartaoModel({
+    required this.id,
     required this.number,
     required this.owner,
     required this.limit,
-    required this.bestDay
+    required this.bestDay,
+    required this.cardColor
   });
 }
