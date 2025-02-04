67ec13d1ab5b7a68553fe870546b7b229de78c6c
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-02-04 15:56:10 -0300

    feat: adição de valores e funções na HomeViewModel

--- lib/app/viewmodel/home_view_model.dart
+++ lib/app/viewmodel/home_view_model.dart
@@ -3,12 +3,12 @@ import 'package:gs3/app/model/cartao_model.dart';
 
 class HomeViewModel extends ChangeNotifier {
   final List<CartaoModel> _cartoes = [
-    CartaoModel(
-        number: "1111 2222", owner: "Cartão 1", limit: 1200.50, bestDay: 20),
-    CartaoModel(
-        number: "3333 4444", owner: "Cartão 2", limit: 1200.50, bestDay: 20),
-    CartaoModel(
-        number: "5555 6666", owner: "Cartão 3", limit: 1200.50, bestDay: 20),
+    CartaoModel(id: 0,
+        number: "1111 2222", owner: "GS3 TECH", limit: 1200.50, bestDay: 20, cardColor: [Color(0xFF2B66BC), Color(0xFF132D55)]),
+    CartaoModel(id: 1,
+        number: "3333 4444", owner: "Jane Doe", limit: 1200.50, bestDay: 20, cardColor: [Color(0xFF017375), Color(0xFF005153)]),
+    CartaoModel(id: 2,
+        number: "5555 6666", owner: "John Doe", limit: 1200.50, bestDay: 20, cardColor: [Color(0xFF8f07bc), Color(0xFF720099)]),
   ];
 
   final ValueNotifier<CartaoModel?> cartaoSelecionado =
@@ -19,4 +19,9 @@ class HomeViewModel extends ChangeNotifier {
   }
 
   List<CartaoModel> get cartoes => _cartoes;
+
+  void selectCard(int index) {
+    cartaoSelecionado.value = cartoes[index];
+    notifyListeners();
+  }
 }
