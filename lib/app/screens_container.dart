e96236fc4ae12d81ba41c0fbd25d9d4a2a0dd3d0
Bernardo de Melo Matuchewski <bernardopgr01@gmail.com>
2025-02-04 11:04:39 -0300

    Criado container de telas para navegação na barra inferior

--- lib/app/screens_container.dart
+++ lib/app/screens_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gs3/app/view/home_screen.dart';
 
 class ScreensContainer extends StatefulWidget {
   const ScreensContainer({super.key});
@@ -8,8 +9,85 @@ class ScreensContainer extends StatefulWidget {
 }
 
 class _ScreensContainerState extends State<ScreensContainer> {
+  int _selectedIndex = 0;
+
+  List<Widget> _appScreens = [
+    HomeScreen(),
+    FaturaScreen(),
+    CartaoScreen(),
+    ShopScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _appScreens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 12,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_copy_outlined),
+            label: 'Fatura',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card_outlined),
+            label: 'Cartão',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Shop',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Color(0xffB4B4B8),
+        unselectedLabelStyle: TextStyle(fontSize: 9, color: Color(0xffB4B4B8)),
+        selectedItemColor: Color(0xFF3C6AB2),
+        selectedLabelStyle: TextStyle(fontSize: 12, color: Color(0xFF3C6AB2)),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class FaturaScreen extends StatelessWidget {
+  const FaturaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Essa página não existe"),
+    );
+  }
+}
+
+class CartaoScreen extends StatelessWidget {
+  const CartaoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Essa página não existe"),
+    );
+  }
+}
+
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Essa página não existe"),
+    );
   }
 }
